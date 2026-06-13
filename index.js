195c5c5e3e86e572de4748beec349e9fe81b4375
CitraSulpiani <citraira32712@gmail.com>
2026-06-13 22:19:30 +0800

    update

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ async function connectDB() {
     return;
   }
 
-  connectPromise = mongoose.connect(mongoURI);
+  connectPromise = mongoose.connect(mongoURL);
   await connectPromise;
   console.log("Database SULSELGO Terkoneksi! ✅");
 }
